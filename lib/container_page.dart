51fb58908daef4958eb32c9a43d0f49c79206c6d
smb <1468287197@qq.com>
2020-06-06 16:23:17 +0800

    添加首页启动图

---
+++ lib/container_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ContainerPage extends StatefulWidget {
+  ContainerPage({Key key}) : super(key: key);
+
+  @override
+  _ContainerPageState createState() => _ContainerPageState();
+}
+
+class _ContainerPageState extends State<ContainerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: Text('data'),
+    );
+  }
+}
---
+++ lib/splash_widget.dart
@@ -0,0 +1,158 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_hello/screen_utils.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:flutter_hello/container_page.dart';
+import 'package:flutter_hello/constant.dart';
+
+class SplashWidget extends StatefulWidget {
+  SplashWidget({Key key}) : super(key: key);
+
+  @override
+  _SplashWidgetState createState() => _SplashWidgetState();
+}
+
+class _SplashWidgetState extends State<SplashWidget> {
+  var container = ContainerPage();
+  bool showAd = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Offstage(
+          child: container,
+          offstage: showAd,
+        ),
+        Offstage(
+            child: Container(
+              color: Colors.white,
+              child: Stack(children: <Widget>[
+                Align(
+                  alignment: Alignment(0.0, 0.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircleAvatar(
+                        radius: ScreenUtils.screenW(context) / 3,
+                        backgroundColor: Colors.white,
+                        backgroundImage:
+                            AssetImage(Constant.ASSETS_IMG + 'home.png'),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 20.0),
+                        child: Text(
+                          '落花有意随流水,流水无心恋落花',
+                          style: TextStyle(fontSize: 15.0, color: Colors.white),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SafeArea(
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Align(
+                            alignment: Alignment.centerRight,
+                            child: Container(
+                              margin: EdgeInsets.only(right: 30.0, top: 20.0),
+                              padding: EdgeInsets.only(
+                                  left: 10.0,
+                                  right: 10.0,
+                                  top: 2.0,
+                                  bottom: 2.0),
+                              child: CountDownWidget(
+                                onCountDownFinishCallBack: (bool value) {
+                                  if (value) {
+                                    setState(() {
+                                      showAd = false;
+                                    });
+                                  }
+                                },
+                              ),
+                              decoration: BoxDecoration(
+                                  color: Color(0xffEDEDED),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10.0))),
+                            )),
+                        Padding(
+                            padding: EdgeInsets.only(bottom: 40.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Image.asset(
+                                  Constant.ASSETS_IMG + 'ic_launcher.png',
+                                  width: 50.0,
+                                  height: 50.0,
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.only(left: 10.0),
+                                  child: Text(
+                                    'Hi,豆芽',
+                                    style: TextStyle(
+                                        color: Colors.green,
+                                        fontSize: 30.0,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                )
+                              ],
+                            ))
+                      ]),
+                )
+              ]),
+              width: ScreenUtils.screenW(context),
+              height: ScreenUtils.screenH(context),
+            ),
+            offstage: !showAd)
+      ],
+    );
+  }
+}
+
+class CountDownWidget extends StatefulWidget {
+  final onCountDownFinishCallBack;
+
+  CountDownWidget({Key key, @required this.onCountDownFinishCallBack})
+      : super(key: key);
+
+  @override
+  _CountDownWidgetState createState() => _CountDownWidgetState();
+}
+
+class _CountDownWidgetState extends State<CountDownWidget> {
+  var _seconds = 6;
+  Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _startTimer();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      '$_seconds',
+      style: TextStyle(fontSize: 17.0),
+    );
+  }
+
+  /// 启动倒计时的计时器。
+  void _startTimer() {
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {});
+      if (_seconds <= 1) {
+        widget.onCountDownFinishCallBack(true);
+        _cancelTimer();
+        return;
+      }
+      _seconds--;
+    });
+  }
+
+  /// 取消倒计时的计时器。
+  void _cancelTimer() {
+    _timer?.cancel();
+  }
+}
